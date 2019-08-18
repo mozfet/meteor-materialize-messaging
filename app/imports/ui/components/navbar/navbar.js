53dd0e62cf572e9d0c18157458db4b0dea1b5204
Wynand Vermeulen <wvermeulen@icscards.nl>
2019-08-18 10:55:04 +0200

    fixed paths and dependancies

--- app/imports/ui/components/navbar/navbar.js
+++ app/imports/ui/components/navbar/navbar.js
@@ -1,2 +1,5 @@
 import 'meteor/mozfet:materialize-messaging'
 import './navbar.html'
+
+export const name = 'navbar'
+Log.log(['debug', 'startup'], `${name}`)
--- app/imports/ui/layouts/body/body.js
+++ app/imports/ui/layouts/body/body.js
@@ -1,2 +1,2 @@
 import './body.html'
-import '/imports/ui/components/navbar/navbar.js'
+import '/imports/ui/components/navbar'
